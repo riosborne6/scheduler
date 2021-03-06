4e7b5155a7c84b263b3f026bee2f79859853f2b4
iano osborne <ianosborne@ians-imac.attlocal.net>
2021-03-06 14:05:26 -0800

    siblings

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 $("#currentDay").text(moment().format("dddd") + " " + moment().format("LL"));
-moment().format("LT");
 function hour() {
-    var hours = moment().hour();
-  $(".time-block").each(function(){
-      blockHour = parseInt($(this).attr("id"))
-      if (hours > blockHour){
-          $(this).addClass("past")
-      }
-      else if (hours === blockHour){
-          $(this).addClass("present")
-          $(this).removeClass("past")
-      }
-      else {
-          $(this).addClass("future")
-          $(this).removeClass("past")
-          $(this).removeClass("present")
-
+  var hours = moment().hour();
+  $(".time-block").each(function () {
+    blockHour = parseInt($(this).attr("id"));
+    if (hours > blockHour) {
+      $(this).addClass("past");
+    } else if (hours === blockHour) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
     }
-  }
+  });
+
   // else if
   // else
 }
+hour();
+
+var save = $(".saveBtn");
+save.on("click", function () {
+  $(this).siblings(".description");
+});
